214a1384ce136b5efd35602a13e577fa1ec45897
Jonl457 <jonl457@yahoo.com>
2015-04-22 19:59:09 -0400

    fixed javascript
    js and css changed

--- popup.js
+++ popup.js
@@ -1,11 +1,17 @@
-function solverFunction(form) {
-     x = parseInt(form.inputLegOne.value);
-     y = parseInt(form.inputLegTwo.value);
-     form.outputHypotenuse.value = Math.sqrt(x*x + y*y);
+function solverFunction() {
+     var x = parseInt(document.getElementById("inputLegOne").value);
+     var y = parseInt(document.getElementById("inputLegTwo").value);
+   	 document.getElementById("text").value = Math.sqrt(x*x + y*y);
     }
-    
 
+function clickHandler(e) {
+	setTimeout(solverFunction, 1000);
+	}
 
-
-
-
+function main() {
+	}
+	
+	document.addEventListener('DOMContentLoaded', function () {
+	document.querySelector('button').addEventListener('click', clickHandler);
+	main();
+	});
